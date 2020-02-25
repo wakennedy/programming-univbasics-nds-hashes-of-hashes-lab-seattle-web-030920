4528a4b1ff84dcb2d5151f76c484f849b40ceed7
William Kennedy <alexkenned@gmail.com>
2020-02-25 20:03:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,13 +32,13 @@
 
 def naming_system
   {
-  Kingdom  {
-    :Phylum = {
-      :Class = {
-        :Order = {
-          :Family = {
-            :Genus = {
-              :Species = {
+  Kingdom => {
+    Phylum => {
+      Class => {
+        Order => {
+          Family => {
+            Genus => {
+              Species => {
                 
               }
             }
