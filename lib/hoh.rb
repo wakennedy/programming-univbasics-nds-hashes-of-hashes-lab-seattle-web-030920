851bffc2e10486251b30c35d80ddb4f2dc21fcda
William Kennedy <alexkenned@gmail.com>
2020-02-25 20:04:10 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,7 @@
 # }
 
 def naming_system
+  {
   Kingdom => {
     Phylum => {
       Class => {
@@ -48,7 +49,7 @@ def naming_system
   }
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+}
 end
 
 
